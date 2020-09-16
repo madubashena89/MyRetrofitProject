571caefe7ea784c38df1f044fd25cae5e59cbf40
madubashena89 <madubashena89@gmail.com>
2020-09-15 21:35:40 -0500

    more request methods added

--- app/src/main/java/com/devtides/retrofitproject/model/ApiCall.kt
+++ app/src/main/java/com/devtides/retrofitproject/model/ApiCall.kt
@@ -2,9 +2,28 @@ package com.devtides.retrofitproject.model
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+import retrofit2.http.QueryMap
 
 interface ApiCall {
 
     @GET("apiCall")
     fun callGet(): Call<ApiCallResponse>
+
+    @POST("apiCall")
+    fun callPost(): Call<ApiCallResponse>
+
+
+    @GET("apiCall?name=Alex&age=24")
+    fun callQueryStatic(): Call<ApiCallResponse>
+
+    @GET("apiCall")
+    fun callQueryDynamic(@Query("name") n: String, @Query("age") a : Int): Call<ApiCallResponse>
+
+    @GET("apiCall")
+    fun callQueryDynamicNullable(@Query("name") n: String?, @Query("age") a : Int?): Call<ApiCallResponse>
+
+    @GET("apiCall")
+    fun callQueryMultiple(@QueryMap params: Map<String, String>): Call<ApiCallResponse>
 }
--- app/src/main/java/com/devtides/retrofitproject/model/ApiCallService.kt
+++ app/src/main/java/com/devtides/retrofitproject/model/ApiCallService.kt
@@ -1,18 +1,38 @@
 package com.devtides.retrofitproject.model
 
+import com.devtides.retrofitproject.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiCallService {
 
     private val BASE_URL = "https://us-central1-apis2-e78c3.cloudfunctions.net/"
+    val okHttp2Client = OkHttpClient.Builder()
+
+    init {
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+        if (BuildConfig.DEBUG){
+            okHttp2Client.addInterceptor(logging)
+        }
+
+    }
 
     private val api = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttp2Client.build())
         .build()
         .create(ApiCall::class.java)
 
-    fun call() = api.callGet()
+    fun call() =
+        //api.callGet()
+        //api.callPost()
+        //api.callQueryStatic()
+        //api.callQueryDynamic("James", 27)
+        //api.callQueryDynamicNullable("James", null)
+        api.callQueryMultiple(hashMapOf(Pair("a", "adfb"), Pair("b", "value2")))
 
 }
